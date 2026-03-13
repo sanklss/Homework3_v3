b49e8dce29f4b4d6b246476f81963a27a72ed595
sanklss <sanklsswt2@yandex.ru>
2026-03-13 19:19:31 +0300

    Добавлен Application для создания консольного меню с возможностью выбора создать пользователя/посмотреть количество пользователей/выйти

---
+++ Homework3_v3/Applications/Application.cs
@@ -0,0 +1,47 @@
+﻿namespace Homework3_v3.Applications
+{
+    public class Application
+    {
+        private ApplicationView _applicationView;
+
+        public Application()
+        {
+            _applicationView = new ApplicationView();
+        }
+
+        public void Run()
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                _applicationView.ShowMenu();
+
+                MenuCommand? command = _applicationView.GetMenuCommand();
+
+                switch (command.Value)
+                {
+                    case MenuCommand.CreateUser:
+                        CreateUser();
+                        break;
+
+                    case MenuCommand.ShowStatistics:
+                        string count = UserFactory.GetUsersCounts();
+                        _applicationView.ShowUsersCount(count);
+                        break;
+
+                    case MenuCommand.Exit:
+                        isRunning = false;
+                        break;
+                }
+            }
+        }
+
+        private void CreateUser()
+        {
+            UserRole? userRole = _applicationView.GetUserRole();
+
+            UserFactory.CreateUser(userRole.Value);
+        }
+    }
+}
