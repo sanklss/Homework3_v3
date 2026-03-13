a35d2fc817912f58162d17b8cefc6fc3afdcb8c5
sanklss <sanklsswt2@yandex.ru>
2026-03-13 19:21:19 +0300

    Добавлен Program.cs для запуска приложения

---
+++ Homework3_v3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Homework3_v3.Applications;
+
+namespace Homework3_v3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Application application = new Application();
+            application.Run();
+        }
+    }
+}
